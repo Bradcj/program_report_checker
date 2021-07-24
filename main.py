7f9d3c6ed9b71487d6ec0790d910227e8547cf48
BradJ <j.chaiyaphop@gmail.com>
2021-07-25 00:50:55 +0700

    Adjust Code

--- main.py
+++ main.py
@@ -20,16 +20,16 @@ class Check_Report:
                 try:
                     df_date = df[df['month'] == date]
                     df_pivot = df_date.pivot(index = 'category', columns = 'month', values = ['brand_sales', 'cate_sales'])
-                    df_pivot['market_share'] = (df_pivot['brand_sales'][date])/(df_pivot['cate_sales'][date])
-                    sum_pct_share = (df_pivot['brand_sales'][date].sum())/(df_pivot['cate_sales'][date].sum())
+                    df_pivot['market_share'] = (df_pivot['brand_sales'][date]) / (df_pivot['cate_sales'][date])
+                    sum_pct_share = (df_pivot['brand_sales'][date].sum()) / (df_pivot['cate_sales'][date].sum())
                     df_pivot['diff_share'] = sum_pct_share - df_pivot['market_share']
                     diff_share_positive = df_pivot[df_pivot['diff_share'] > 0]
-                    diff_share_positive['sum_share'] = (diff_share_positive['brand_sales'][date])*diff_share_positive['diff_share']
+                    diff_share_positive['sum_share'] = (diff_share_positive['brand_sales'][date]) * diff_share_positive['diff_share']
                     fair_share = diff_share_positive['sum_share'].sum().round(2)
                     fair_share = str(fair_share)
                     table = Table(title='[bold][yellow]Check Report[/yellow][/bold]', style='cyan')
-                    table.add_column('[yellow]Date[/yellow]', justify='right', style='green')
-                    table.add_column('[yellow]Fair Share[/yellow]', justify='right', style='green')
+                    table.add_column('[yellow]Date[/yellow]', justify = 'right', style = 'green')
+                    table.add_column('[yellow]Fair Share[/yellow]', justify = 'right', style = 'green')
                     table.add_row(date, fair_share)
                     console = Console()
                     console.print(table)
